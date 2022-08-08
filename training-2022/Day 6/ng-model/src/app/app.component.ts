ed9fc611b6a537abbd5ba1618beed0e5b2b56012
ghineaal <alin.ghinea@kronsoft.ro>
2022-08-08 11:02:45 +0300

    remove e2e

---
+++ training-2022/Day 6/ng-model/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ng-model';
+  firstName: string = '';
+  lastName: string = '';
+
+  submitForm(form: NgForm): void {
+    this.lastName = form.value.lastName;
+    this.firstName = form.value.firstName;
+  }
+}
